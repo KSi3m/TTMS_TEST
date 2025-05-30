1112fb7093a735985e9a92dd8150edb195dbbab6
ksi3m <mieszko9919@gmail.com>
2025-05-30 09:10:37 +0200

    entitity and dto

---
+++ TTMS/Dtos/CreateTelephoneDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TTMS.Dtos
+{
+    public class CreateTelephoneDto
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+    }
+}
---
+++ TTMS/Entities/Telephone.cs
@@ -0,0 +1,10 @@
+﻿namespace TTMS.Entities
+{
+    public class Telephone
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+    }
+}
