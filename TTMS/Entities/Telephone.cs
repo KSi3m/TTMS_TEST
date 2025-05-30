7930bb4d7bdbac9dde82f24bab090fd90a2c1e87
ksi3m <mieszko9919@gmail.com>
2025-05-30 09:36:55 +0200

    angular app init

--- TTMS/Entities/Telephone.cs
+++ TTMS/Entities/Telephone.cs
@@ -1,10 +1,14 @@
-﻿namespace TTMS.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TTMS.Entities
 {
     public class Telephone
     {
 
         public int Id { get; set; }
+        [StringLength(15)]
         public string Name { get; set; }
+        [RegularExpression("^\\d{9}$", ErrorMessage = "You must supply 9 digits")]
         public string Number { get; set; }
     }
 }
